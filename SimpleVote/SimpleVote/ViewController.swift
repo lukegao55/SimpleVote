30510e77de652d4a207475fb1b41cc5cfc0f4c62
Luke Gao <>
2018-10-22 09:51:36 -0700

    Create project

---
+++ SimpleVote/SimpleVote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleVote
+//
+//  Created by Luke Gao on 10/22/18.
+//  Copyright © 2018 Luke Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
